2941fc478c8d4708db5db2023487359721d011ad
mohit-ps <mohit.pandey@publicissapent.com>
2022-11-17 22:04:00 +0530

    apply polyfill added

--- apply.js
+++ apply.js
@@ -4,17 +4,18 @@ let person = {
 };
 
 let personDetail = function (age, year) {
+  console.log(this, "this");
   console.log(
     `${this.name} lives in ${this.country} and his age is ${age} and year is ${year}`
   );
 };
 
-// Call Polyfill
-// Takes the context as an argument and following args which can be anything
-
-Function.prototype.myCall = function (context = {}, ...args) {
+Function.prototype.myApply = function (context = {}, args = []) {
+  if (!Array.isArray(args)) {
+    throw new Error("Passed Args is not Array");
+  }
   context.fn = this;
   context.fn(...args);
 };
 
-personDetail.myCall(person, 42, 2022);
+personDetail.myApply(person, [27, 2022]);
